9f385273df38c652ec262e866476f12e8b192249
Soumya Shetty <soumya.shetty@cerner.com>
2014-09-17 16:20:13 -0500

    Initial commot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeeTheWorld::Application.config.secret_key_base = '8dbc9cf62b7a81080aa38aadb818c986f9403483a40332e89a8e137ffe778973b94b96cdba7f0b4d9c67a96b5994ee29ae9b56be516b8ab83e330e1c998d61ce'
